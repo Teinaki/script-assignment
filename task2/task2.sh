34733e4e435331111e6e119ce10c9bd0855d8b68
Teinaki McFelin <MCFETK1@student.op.ac.nz>
2021-06-07 22:47:31 +1200

    added working scp command added a few comments

--- task2/task2.sh
+++ task2/task2.sh
@@ -4,28 +4,33 @@ input=$1
 #if no argument is provided
 if [ -z "$1" ]
 then
-        echo "Enter Directory"
+        echo "Enter full path to directory that is being backed up"
         read input
 fi
 
-#if the dictory exists
+#if directory exists
 if [ -d "$input" ]
 then
-        echo "Creating backup tar in the users home directory"
-        tar -czvf ~/backup.tar.gz $input
+        #get the name of the final directory, ommiting everything before '/'
+        enddir=${input##*/}
+        echo "Creating Tar of directory in the users home directory"
+        #makes tar with the name of the directory being backed up
+        tar -czvf ~/$enddir.tar.gz $input
 else
         echo "Error that directory doesn't exist"
         exit 1
 fi
 
+#user input for the securely copy file
 echo "Enter IP address or URL of the remote server"
 read URL
 echo "Enter Port number of the remote server"
 read PORT
+echo "Enter target user on the remote server"
+read USER
+echo "Enter full path for target directory to save to"
+read DIR
 
-if [ -z "$1" ]
-then
-        echo "Target Directory is $input URL is $URL Port number is $PORT"
-else
-        echo "Target Directory is $1 URL is $URL Port number is $PORT"
-fi
+#will take the user data just added and attempt to copy the tar.gz to there
+scp -P $PORT ~/$enddir.tar.gz $USER@$URL:$DIR
+#most likely will ask for login details on the remote host
